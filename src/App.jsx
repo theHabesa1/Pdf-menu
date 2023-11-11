b47b61f84c24e561085f8a5f4b718725199c6891
theHabesa1 <yaredabera215@gmail.com>
2023-11-11 19:57:10 +0300

    pdf iframe

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Document, Page } from 'react-pdf';
+import pdfFile from './assets/content.pdf';
+
+const App = () => {
+  
+
+  return (
+    <div>
+     <iframe src="https://www.slideshare.net/slideshow/embed_code/key/BcV83HaTdSdR8S?hostedIn=slideshare&page=upload" width="550" height="1050" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"  ></iframe>
+    </div>
+  );
+};
+
+export default App;
