93a309f620aed2fd9ed081107406ec4e543ecc43
theHabesa1 <yaredabera215@gmail.com>
2023-11-22 15:47:21 +0300

    Yod fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Resturaone from './page/resturaone';
+import Yod from './page/yod';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/vadmas" element={<Resturaone />} />
+      <Route path="/yod" element={<Yod />} />
     </Routes>
   );
 };
---
+++ src/page/yod.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Yod = () => {
+  return (
+    <div>
+     <iframe src="https://www.slideshare.net/slideshow/embed_code/key/bK2KcRGJM60RY?hostedIn=slideshare&page=upload" width="476" height="400" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+    </div>
+  );
+};
+
+export default Yod;
