336122e213d666f48f6feaa623e2d4c87b875ea8
theHabesa1 <yaredabera215@gmail.com>
2023-11-25 20:11:13 +0300

    pdf fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Resturaone from './page/resturaone';
 import Yod from './page/yod';
+import YodLun from './page/yodlunch';
+import YodDin from './page/yoddin';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/vadmas" element={<Resturaone />} />
       <Route path="/yod" element={<Yod />} />
+      <Route path="/yodlun" element={<YodLun />} />
+      <Route path="/yoddin" element={<YodDin />} />
     </Routes>
   );
 };
--- src/page/yod.jsx
+++ src/page/yod.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Yod = () => {
   return (
     <div>
-     <iframe src="https://www.slideshare.net/slideshow/embed_code/key/bK2KcRGJM60RY?hostedIn=slideshare&page=upload" width="476" height="400" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+     <iframe src="https://www.slideshare.net/slideshow/embed_code/key/KodgLm5GWd2CF4?hostedIn=slideshare&page=upload" width="470" height="900" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
     </div>
   );
 };
